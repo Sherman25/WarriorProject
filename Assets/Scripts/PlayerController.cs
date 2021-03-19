535d8ebd18d281e66771fc03e40aada35472b08f
Sherman25 <n.v.sherman@gmail.com>
2021-03-19 11:38:17 +0200

    init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Animator animator;
+    
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    Vector2 movement;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+        movement = new Vector2(moveHorizontal, moveVertical);
+        animator.SetFloat("Horizontal", moveHorizontal);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * Time.fixedDeltaTime);
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 4)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
